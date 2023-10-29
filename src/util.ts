dedf6f890a4578b608444be31d5f6c312be2b0d1
4MBL <74970679+4MBL@users.noreply.github.com>
2023-10-29 20:12:40 +0200

    use switch

--- src/util.ts
+++ src/util.ts
@@ -40,66 +40,90 @@ export function simplifyProps(data: any) {
   for (const item of data) {
     if (!item.properties) continue;
     for (const prop in item.properties) {
-      if (item.properties[prop].type === 'title') {
-        item.title = item.properties[prop].title[0].plain_text;
-      } else if (item.properties[prop].type === 'select') {
-        item[prop] = item.properties[prop].select?.name;
-      } else if (item.properties[prop].type === 'multi_select') {
-        item[prop] = item.properties[prop].multi_select?.map(
-          (option: any) => option.name
-        );
-      } else if (item.properties[prop].type === 'formula') {
-        item[prop] = item.properties[prop].formula?.string;
-      } else if (item.properties[prop].type === 'relation') {
-        item[prop] = item.properties[prop].relation?.map(
-          (relation: any) => relation.id
-        );
-      } else if (item.properties[prop].type === 'rollup') {
-        item[prop] = item.properties[prop].rollup?.array?.map(
-          (relation: any) => relation.id
-        );
-      } else if (item.properties[prop].type === 'checkbox') {
-        item[prop] = item.properties[prop].checkbox;
-      } else if (item.properties[prop].type === 'rich_text') {
-        item[prop] = item.properties[prop].rich_text[0]?.plain_text;
-      } else if (item.properties[prop].type === 'number') {
-        item[prop] = item.properties[prop].number;
-      } else if (item.properties[prop].type === 'date') {
-        item[prop] = {
-          start: item.properties[prop].date?.start,
-          end: item.properties[prop].date?.end,
-        };
-      } else if (item.properties[prop].type === 'people') {
-        item[prop] = item.properties[prop].people?.map(
-          (person: any) => person.id
-        );
-      } else if (item.properties[prop].type === 'files') {
-        item[prop] = item.properties[prop].files?.map(
-          (file: any) => file.file.url
-        );
-      } else if (item.properties[prop].type === 'status') {
-        item[prop] = item.properties[prop].status.name;
-      } else if (item.properties[prop].type === 'unique_id') {
-        item[prop] =
-          item.properties[prop].unique_id.prefix +
-          item.properties[prop].unique_id.number;
-      } else if (item.properties[prop].type === 'url') {
-        item[prop] = item.properties[prop].url;
-      } else if (item.properties[prop].type === 'email') {
-        item[prop] = item.properties[prop].email;
-      } else if (item.properties[prop].type === 'phone_number') {
-        item[prop] = item.properties[prop].phone_number;
-      } else if (item.properties[prop].type === 'created_time') {
-        item[prop] = item.properties[prop].created_time;
-      } else if (item.properties[prop].type === 'created_by') {
-        item[prop] = item.properties[prop].created_by.id;
-      } else if (item.properties[prop].type === 'last_edited_time') {
-        item[prop] = item.properties[prop].last_edited_time;
-      } else if (item.properties[prop].type === 'last_edited_by') {
-        item[prop] = item.properties[prop].last_edited_by.id;
-      } else {
-        continue;
+      switch (item.properties[prop].type) {
+        case 'title':
+          item.title = item.properties[prop].title[0].plain_text;
+          break;
+        case 'rich_text':
+          item[prop] = item.properties[prop].rich_text[0]?.plain_text;
+          break;
+        case 'number':
+          item[prop] = item.properties[prop].number;
+          break;
+        case 'select':
+          item[prop] = item.properties[prop].select?.name;
+          break;
+        case 'multi_select':
+          item[prop] = item.properties[prop].multi_select?.map(
+            (option: any) => option.name
+          );
+          break;
+        case 'status':
+          item[prop] = item.properties[prop].status.name;
+          break;
+        case 'date':
+          item[prop] = {
+            start: item.properties[prop].date?.start,
+            end: item.properties[prop].date?.end,
+          };
+          break;
+        case 'people':
+          item[prop] = item.properties[prop].people?.map(
+            (person: any) => person.id
+          );
+          break;
+        case 'files':
+          item[prop] = item.properties[prop].files?.map(
+            (file: any) => file.file.url
+          );
+          break;
+        case 'checkbox':
+          item[prop] = item.properties[prop].checkbox;
+          break;
+        case 'url':
+          item[prop] = item.properties[prop].url;
+          break;
+        case 'email':
+          item[prop] = item.properties[prop].email;
+          break;
+        case 'phone_number':
+          item[prop] = item.properties[prop].phone_number;
+          break;
+        case 'formula':
+          item[prop] = item.properties[prop].formula?.string;
+          break;
+        case 'relation':
+          item[prop] = item.properties[prop].relation?.map(
+            (relation: any) => relation.id
+          );
+          break;
+        case 'rollup':
+          item[prop] = item.properties[prop].rollup?.array?.map(
+            (relation: any) => relation.id
+          );
+          break;
+        case 'created_time':
+          item[prop] = item.properties[prop].created_time;
+          break;
+        case 'created_by':
+          item[prop] = item.properties[prop].created_by.id;
+          break;
+        case 'last_edited_time':
+          item[prop] = item.properties[prop].last_edited_time;
+          break;
+        case 'last_edited_by':
+          item[prop] = item.properties[prop].last_edited_by.id;
+          break;
+        case 'unique_id':
+          item[prop] =
+            item.properties[prop].unique_id.prefix +
+            item.properties[prop].unique_id.number;
+          break;
+
+        default:
+          continue;
       }
+
       delete item.properties[prop];
       if (Object.keys(item.properties).length === 0) delete item.properties;
     }
