f02c7b7fd8dd433df05fe1e38fdeae25a35f801d
4MBL <74970679+4MBL@users.noreply.github.com>
2024-02-13 14:47:35 +0200

    fix `queryDatabaseFull` to return all results when setting `simplyfyProps: true`

--- src/api.ts
+++ src/api.ts
@@ -54,7 +54,10 @@ export async function queryDatabase(
     ...params,
     filter: options?.filter as any,
   });
-  return processData(data.results, options?.propOptions);
+  return {
+    data: processData(data.results, options?.propOptions),
+    cursor: data.next_cursor,
+  };
 }
 
 export async function queryDatabaseFull(id: string, options?: QueryOptions) {
@@ -68,9 +71,9 @@ export async function queryDatabaseFull(id: string, options?: QueryOptions) {
 
   do {
     const response = await queryDatabase(id, nextCursor, options);
-    nextCursor = response.next_cursor ?? undefined;
-    allResults.push(...response);
-  } while (nextCursor !== undefined && nextCursor !== null);
+    nextCursor = response.cursor ?? undefined;
+    allResults.push(...response.data);
+  } while (nextCursor);
   return allResults;
 }
 
