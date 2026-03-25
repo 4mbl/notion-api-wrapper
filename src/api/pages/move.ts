1a724b027b483ad408c30dc6ed2ec29927415a0b
4MBL <74970679+4mbl@users.noreply.github.com>
2026-03-25 22:02:05 +0200

    Support page move API (#353)

---
+++ src/api/pages/move.ts
@@ -0,0 +1,43 @@
+import { getApiKey } from '../../auth.js';
+import { NOTION_VERSION } from '../../constants.js';
+import {
+  NotionError,
+  type NotionErrorResponse,
+} from '../../internal/errors.js';
+import type * as Notion from '../../notion-types.js';
+import { validateApiVersion, validateObjectId } from '../../validation.js';
+
+export async function movePage(
+  /** Notion page id. */
+  id: string,
+  body: Omit<Notion.MovePageParameters, 'page_id'>,
+  options?: {
+    notionToken?: string;
+    notionVersion?: string;
+  },
+) {
+  validateObjectId(id);
+  validateApiVersion(options?.notionVersion);
+
+  const apiKey = getApiKey(options);
+
+  const response = await fetch(`https://api.notion.com/v1/pages/${id}/move`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${apiKey}`,
+      'Notion-Version': options?.notionVersion ?? NOTION_VERSION,
+    },
+    body: JSON.stringify(body),
+  });
+
+  if (!response.ok) {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const errorJson: NotionErrorResponse = (await response.json()) as any;
+    throw new NotionError(errorJson);
+  }
+
+  return response.json() as Promise<
+    Notion.PageObjectResponse | Notion.PartialPageObjectResponse
+  >;
+}
